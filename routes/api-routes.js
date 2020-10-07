547dda4cf581977f954525ddcd60a6abea58386d
phantomramen <ali.hernandez9two@gmail.com>
2020-10-06 19:05:39 -0600

    checkout

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -10,22 +10,26 @@ module.exports = function(app) {
   // Otherwise the user will be sent an error
   app.get("/api/quote", function(req, res) {
     var settings = {
-
       headers: {
         "x-rapidapi-host":
           "quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com",
         "x-rapidapi-key": process.env.SECRET_KEY,
-      }
+      },
     };
-    axios.get("https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote?token=ipworld.info",settings).then(function(results){
-      res.json(results.data)
-    })
+    axios
+      .get(
+        "https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote?token=ipworld.info",
+        settings
+      )
+      .then(function(results) {
+        res.json(results.data);
+      });
   });
   app.post("/api/login", passport.authenticate("local"), (req, res) => {
     // Sending back a password, even a hashed password, isn't a good idea
     res.json({
       email: req.user.email,
-      id: req.user.id
+      id: req.user.id,
     });
   });
 
@@ -35,12 +39,12 @@ module.exports = function(app) {
   app.post("/api/signup", (req, res) => {
     db.User.create({
       email: req.body.email,
-      password: req.body.password
+      password: req.body.password,
     })
       .then(() => {
         res.redirect(307, "/api/login");
       })
-      .catch(err => {
+      .catch((err) => {
         res.status(401).json(err);
       });
   });
@@ -61,7 +65,7 @@ module.exports = function(app) {
       // Sending back a password, even a hashed password, isn't a good idea
       res.json({
         email: req.user.email,
-        id: req.user.id
+        id: req.user.id,
       });
     }
   });
