88d284acaa77ba2e7b0c833a54a9381ac7f8fb35
Joel Terrell <falconreid@hotmail.com>
2020-10-06 20:57:38 -0600

    api-routes update from Ali

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,6 @@
 $(document).ready(function() {
-  const queryURL =
-    "https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote/?rapidapi-key=350912590emshd6ab06b9aef56ecp1ddaa6jsn5bbc1034b7fd";
-  var quote = $("#inQuote").val();
-  $("#inQuote").append(quote);
-  // $("#name").empty();
-  //console.log(response);
-  console.log(queryURL);
-
   $.ajax({
-    url: queryURL,
+    url: "/api/quote",
     method: "GET",
   }).then(function(response) {
     console.log(response);
@@ -16,7 +8,7 @@ $(document).ready(function() {
     //$("#inQuote").empty();
     var quote = $("<p>")
       .addClass("inQuote")
-      .text(response.text);
+      .text(response.text + '"');
     $("#inQuote").append(quote);
     var author = $("<h3>")
       .addClass("author")
